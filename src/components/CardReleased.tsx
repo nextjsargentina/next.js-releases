d4cd5db7ed146b989fe1c721f3eac4405706061d
Emanuel Peire <emapeire@outlook.com>
2024-01-11 18:09:39 -0300

    Add link to release name in CardReleased component

--- src/components/CardReleased.tsx
+++ src/components/CardReleased.tsx
@@ -8,9 +8,11 @@ import {
 	CardHeader,
 	CardTitle
 } from '@/components/ui/card'
+import Link from 'next/link'
 
 export function CardReleased({ releases }: { releases: Release[] }) {
 	const noReleases = releases.length === 0
+	const prerelease = releases[0].prerelease ? 'prerelease' : 'release'
 
 	return (
 		<div>
@@ -26,8 +28,17 @@ export function CardReleased({ releases }: { releases: Release[] }) {
 						<div key={release.id}>
 							<Card className='flex items-center mb-12'>
 								<CardHeader>
-									{release.name}
-									{/* <p>{release.body}</p> */}
+									<CardTitle className='flex flex-row gap-x-2'>
+										<Link
+											href={release.html_url}
+											className='text-blue-500'
+											rel='noopener noreferrer'
+											target='_blank'
+										>
+											{release.name}
+										</Link>
+										<span className='text-gray-500'>{release.prerelease}</span>
+									</CardTitle>
 								</CardHeader>
 							</Card>
 						</div>
@@ -39,17 +50,11 @@ export function CardReleased({ releases }: { releases: Release[] }) {
 					<CardTitle>Card Title</CardTitle>
 					<CardDescription>Card Description</CardDescription>
 				</CardHeader>
+
 				<CardContent>
 					<p>Card Content</p>
-					<div>
-						{releases.map((release) => (
-							<div key={release.id}>
-								<h3>{release.name}</h3>
-								<p>{release.body}</p>
-							</div>
-						))}
-					</div>
 				</CardContent>
+
 				<CardFooter>
 					<p>Card Footer</p>
 				</CardFooter>
