19eac5f4cfbd82d5d87ab5986903ffca0245ebcf
Emanuel Peire <emapeire@outlook.com>
2024-01-11 16:17:22 -0300

    Update page component and remove Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import { Card } from '@/components/Card'
+import { CardReleased } from '@/components/CardReleased'
 import { getReleases } from '@/lib/getReleases'
-import { release } from 'os'
 
 export default async function Home() {
 	const releases = await getReleases()
 
 	return (
 		<main className='flex min-h-screen flex-col items-center p-24'>
-			<h1>NextJS Releases Bot</h1>
-			<Card releases={releases} />
+			<h1 className='flex justify-center items-center font-bold text-3xl mb-12'>
+				NextJS Releases Bot
+			</h1>
+			<CardReleased releases={releases} />
 		</main>
 	)
 }
---
+++ src/components/CardReleased.tsx
@@ -0,0 +1,59 @@
+import { getReleases } from '@/lib/getReleases'
+import { Release } from '@/types'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle
+} from '@/components/ui/card'
+
+export function CardReleased({ releases }: { releases: Release[] }) {
+	const noReleases = releases.length === 0
+
+	return (
+		<div>
+			{noReleases ? (
+				<section>
+					<Card>
+						<CardTitle>No releases</CardTitle>
+					</Card>
+				</section>
+			) : (
+				<section>
+					{releases.map((release) => (
+						<div key={release.id}>
+							<Card>
+								<CardHeader>
+									{release.name}
+									{/* <p>{release.body}</p> */}
+								</CardHeader>
+							</Card>
+						</div>
+					))}
+				</section>
+			)}
+			{/* <Card>
+				<CardHeader>
+					<CardTitle>Card Title</CardTitle>
+					<CardDescription>Card Description</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<p>Card Content</p>
+					<div>
+						{releases.map((release) => (
+							<div key={release.id}>
+								<h3>{release.name}</h3>
+								<p>{release.body}</p>
+							</div>
+						))}
+					</div>
+				</CardContent>
+				<CardFooter>
+					<p>Card Footer</p>
+				</CardFooter>
+			</Card> */}
+		</div>
+	)
+}
