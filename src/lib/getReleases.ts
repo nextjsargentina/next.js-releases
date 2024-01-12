5c3ff763df82ec30f9dafe407308bb4655711ffc
Emanuel Peire <emapeire@outlook.com>
2024-01-12 17:02:00 -0300

    Update API call to use axios

--- src/lib/getReleases.ts
+++ src/lib/getReleases.ts
@@ -1,17 +1,20 @@
+import axios from 'axios'
 import { type Release } from '../types'
 import { personalAccessToken } from '@/config'
-import { Octokit } from '@octokit/core'
 
-const octokit = new Octokit({ auth: `${personalAccessToken}` })
-
-export async function getReleases(): Promise<Release[] | void> {
+export const getReleases = async (): Promise<Release[]> => {
 	try {
-		const response = await octokit.request('GET /repos/{owner}/{repo}/releases', {
-			owner: 'vercel',
-			repo: 'next.js'
-		})
-		console.log(response.data)
+		const response = await axios.get<Release[]>(
+			'https://api.github.com/repos/vercel/next.js/releases',
+			{
+				headers: {
+					Authorization: `token ${personalAccessToken}`
+				}
+			}
+		)
+		return response.data
 	} catch (error) {
-		console.error('Error obtaining the releases:', error)
+		console.error(error)
+		return []
 	}
 }
