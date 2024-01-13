f8743e3fec96fac293d72e284c7be6de08b3f5bf
Emanuel Peire <emapeire@outlook.com>
2024-01-13 00:06:34 -0300

    Remove dompurify dependency

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,5 @@
 import { marked } from 'marked'
-import DOMPurify from 'dompurify'
 
 export async function markdownToHtml(markdown: string) {
 	const html = await marked(markdown)
-	return DOMPurify.sanitize(html)
 }
