a110afacaac95d49bd49338aebbbfbc5f1018fe7
Emanuel Peire <emapeire@outlook.com>
2024-01-14 21:23:11 -0300

    Refactor function parameter name in getReleases.ts and create useReleases.ts

--- src/lib/getReleases.ts
+++ src/lib/getReleases.ts
@@ -2,12 +2,12 @@ import axios from 'axios'
 import { type Release } from '../types'
 import { personalAccessToken } from '@/config'
 
-interface QueryParams {
+interface SearchParams {
 	page: number
 	perPage: number
 }
 
-export async function getReleases({ page, perPage }: QueryParams): Promise<Release[]> {
+export async function getReleases({ page, perPage }: SearchParams): Promise<Release[]> {
 	try {
 		const response = await axios.get<Release[]>(
 			`https://api.github.com/repos/vercel/next.js/releases?page=${page}&per_page=${perPage}`,
