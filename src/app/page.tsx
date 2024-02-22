9e0a126b74da9475e004fddeff6b1312a05d120a
Emanuel Peire <emapeire@outlook.com>
2024-02-22 16:12:35 -0300

    Update target to esnext in tsconfig.json and refactor Home component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
-'use client'
-
 import { CardReleased } from '@/components/CardReleased'
 import { PaginationControl } from '@/components/PaginationControl'
-import useReleases from '@/hooks/useReleases'
+import { getReleases } from '@/lib/getReleases'
 
-export default function Home() {
-	const { releases, loading, error } = useReleases()
-
-	if (loading) {
-		return <div>Loading...</div>
-	}
-
-	if (error) {
-		return <div>Error: {error.message}</div>
-	}
-
+export default async function Home() {
+	const releases = await getReleases({ page: 1, perPage: 10 })
 	return (
 		<main className='flex min-h-screen flex-col items-center p-24'>
 			<h1 className='flex justify-center items-center text-center font-bold md:text-3xl text-2xl mb-12'>
-				NextJS Releases Bot
+				NextJS Releases App
 			</h1>
 			<CardReleased releases={releases} />
 			{/* <PaginationControl /> */}
--- src/hooks/useReleases.ts
+++ src/hooks/useReleases.ts
@@ -1,39 +1,4 @@
-'use client'
-
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
 import { getReleases } from '@/lib/getReleases'
 import { type Release } from '../types'
 
-export default function useReleases() {
-	const [releases, setReleases] = useState<Release[]>([])
-	const [loading, setLoading] = useState(false)
-	const [error, setError] = useState<Error | null>(null)
-	const router = useRouter()
-
-	useEffect(() => {
-		const page = parseInt(router.query.page as string) || 1
-		const perPage = parseInt(router.query.perPage as string) || 5
-
-		if (!router.isReady) return
-
-		const fetchReleases = async () => {
-			setLoading(true)
-			setError(null)
-			try {
-				const data = await getReleases({ page, perPage })
-				setReleases(data)
-			} catch (error) {
-				setError(error as Error)
-			} finally {
-				setLoading(false)
-			}
-		}
-
-		if (router.isReady) {
-			fetchReleases()
-		}
-	}, [router.isReady, router.query.page, router.query.perPage])
-
-	return { releases, loading, error }
-}
+export default function useReleases() {}
