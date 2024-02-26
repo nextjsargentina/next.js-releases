32cfd3325e78ad0e5693a54ea9f63e21a1a5b76b
Emanuel Peire <emapeire@outlook.com>
2024-02-26 15:18:40 -0300

    Refactor code formatting and remove unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import { CardReleased } from '@/components/CardReleased'
-import { PaginationControl } from '@/components/PaginationControl'
-import { getReleases } from '@/lib/getReleases'
+import Link from 'next/link'
 
-export default async function Home() {
-	const releases = await getReleases({ page: 1, perPage: 10 })
+export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center p-24'>
 			<h1 className='flex justify-center items-center text-center font-bold md:text-3xl text-2xl mb-12'>
 				NextJS Releases App
 			</h1>
-			<CardReleased releases={releases} />
-			{/* <PaginationControl /> */}
+			<Link href='/releases' />
 		</main>
 	)
 }
--- src/hooks/useReleases.ts
+++
@@ -1,4 +0,0 @@
-import { getReleases } from '@/lib/getReleases'
-import { type Release } from '../types'
-
-export default function useReleases() {}
