fcc479ca899608f283dc7b3c2aad9a120dc3ac6b
Emanuel Peire <emapeire@outlook.com>
2024-02-26 16:30:18 -0300

    Refactor home page layout and button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center min-h-screen p-24'>
-      <h1 className='flex justify-center items-center text-center font-bold text-5xl mb-12'>
+      <h1 className='flex justify-center items-center md:text-center text-balance font-bold text-5xl my-6'>
         NextJS Releases App
       </h1>
-      <Link href='/releases'>
-        <Button>View Releases</Button>
-      </Link>
+
+      <section className='flex w-full md:w-fit my-6'>
+        <Link href='/releases'>
+          <Button className='text-base font-mono font-semibold'>
+            View Releases
+          </Button>
+        </Link>
+      </section>
     </main>
   )
 }
