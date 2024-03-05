382ed96280ed4162618c0b96f50a2692e96f4648
Emanuel Peire <emapeire@outlook.com>
2024-03-05 17:58:02 -0300

    Update page and footer styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-center min-h-[95vh] md:p-24 p-12'>
+    <main className='flex flex-col items-center justify-center min-h-[85vh] md:p-24 p-12'>
       <section className='flex flex-col border border-gray-500 rounded-xl shadow-2xl shadow-white/10 p-12 md:px-24'>
-        <h1 className='flex justify-center items-center md:text-center text-balance font-bold text-gray-100 text-5xl my-4'>
-          NextJS Releases App
+        <h1 className='flex justify-center items-center md:text-center text-balance font-bold text-gray-100 text-3xl md:text-4xl my-6'>
+          Check out the latest releases!
         </h1>
 
-        <div className='my-4'>
+        <div className='flex justify-end items-center my-6'>
           <Link href='/releases'>
             <Button className='text-base font-mono font-semibold bg-gray-100 text-gray-900'>
               View releases
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className='flex justify-center items-center text-gray-100 text-sm font-mono'>
+    <footer className='flex justify-center items-center text-gray-100 text-sm font-mono border-t border-gray-500 mt-6 h-12 md:border-none md:mt-2'>
       <p>&copy; {new Date().getFullYear()} NextJS Releases App</p>
       <span className='mx-2'>|</span>
       <p>
