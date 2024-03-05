4c6ca43d4d448d80573721fc8e8febcd09a74bf3
Emanuel Peire <emapeire@outlook.com>
2024-03-05 17:27:55 -0300

    Update header component with GitHub and X links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,28 @@
+import Link from 'next/link'
 import { GitHubIcon } from './icons/github'
 import { XIcon } from './icons/x'
 
 export function Header() {
   return (
-    <header className='flex items-center w-full h-16 border-b border-gray-500'>
-      <h1 className='md:mx-12 mx-6 text-center md:text-xl text-sm font-semibold font-mono text-gray-100'>
+    <header className='flex justify-between items-center w-full h-16 border-b border-gray-500 md:px-12 px-6'>
+      <h1 className='text-center md:text-xl text-base font-semibold font-mono text-gray-100'>
         NextJS Releases App
       </h1>
-      <div>
-        <GitHubIcon className='size-8' />
-        <XIcon className='size-8' />
+      <div className='flex flex-row items-center gap-x-6'>
+        <Link
+          href='https://github.com/nextjsargentina'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <GitHubIcon className='size-7 hover:scale-110 transition-all ease-in-out duration-300' />
+        </Link>
+        <Link
+          href='https://x.com/nextjsargentina'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <XIcon className='size-6 hover:scale-110 transition-all ease-in-out duration-300' />
+        </Link>
       </div>
     </header>
   )
