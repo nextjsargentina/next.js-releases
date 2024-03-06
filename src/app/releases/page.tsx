ab941985a6e54c1d736a31a3e532c5bd8f789fe4
Emanuel Peire <emapeire@outlook.com>
2024-03-06 20:37:16 -0300

    Add PaginationControl component

--- src/app/releases/page.tsx
+++ src/app/releases/page.tsx
@@ -1,11 +1,11 @@
 import { CardReleased } from '@/components/card-released'
-// import { PaginationControl } from '@/components/pagination-control'
+import { PaginationControl } from '@/components/pagination-control'
 
 export default function ReleasesPage() {
   return (
     <main className='flex flex-col items-center justify-center min-h-[85vh] md:p-24 py-12 px-6'>
       <CardReleased />
-      {/* <PaginationControl /> */}
+      <PaginationControl />
     </main>
   )
 }
