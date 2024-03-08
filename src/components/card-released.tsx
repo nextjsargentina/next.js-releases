773f4228420364ea5ece71d4380e1ea551f72d93
Emanuel Peire <emapeire@outlook.com>
2024-03-08 13:59:14 -0300

    Refactor card-released component styling

--- src/components/card-released.tsx
+++ src/components/card-released.tsx
@@ -22,23 +22,23 @@ export function CardReleased({ releases }: { releases: Release[] }) {
   return (
     <main>
       {noReleases ? (
-        <Card className='border-gray-600 shadow-2xl shadow-white/10'>
-          <CardHeader className='flex'>
-            <CardTitle className='flex md:text-3xl text-2xl'>
+        <Card className="border-gray-600 shadow-2xl shadow-white/10">
+          <CardHeader className="flex">
+            <CardTitle className="flex md:text-3xl text-2xl">
               No releases
             </CardTitle>
-            <CardDescription className='text-pretty'>
+            <CardDescription className="text-pretty">
               There are no releases to show. Please check back later.
             </CardDescription>
           </CardHeader>
-          <CardFooter className='flex justify-end'>
+          <CardFooter className="flex justify-end">
             <Link
-              className='hover:underline'
-              href='https://github.com/vercel/next.js/releases'
-              rel='noopener noreferrer'
-              target='_blank'
+              className="hover:underline"
+              href="https://github.com/vercel/next.js/releases"
+              rel="noopener noreferrer"
+              target="_blank"
             >
-              <Button className='font-mono'>View all releases</Button>
+              <Button className="font-mono">View all releases</Button>
             </Link>
           </CardFooter>
         </Card>
@@ -47,16 +47,16 @@ export function CardReleased({ releases }: { releases: Release[] }) {
           {releases.map((release, index) => (
             <Card
               key={release.id}
-              className='mb-12 border-gray-600 max-w-sm md:max-w-full'
+              className="mb-12 border-gray-600 max-w-sm min-w-sm md:max-w-full md:min-w-full"
             >
-              <section className='flex items-center'>
-                <CardHeader className='flex'>
-                  <CardTitle className='flex md:text-3xl text-2xl'>
+              <section className="flex items-center">
+                <CardHeader className="flex">
+                  <CardTitle className="flex md:text-3xl text-2xl">
                     <Link
                       href={release.html_url}
-                      className='text-gray-100 hover:text-blue-500 hover:underline'
-                      rel='noopener noreferrer'
-                      target='_blank'
+                      className="text-gray-100 hover:text-blue-500 hover:underline"
+                      rel="noopener noreferrer"
+                      target="_blank"
                     >
                       {release.name}
                     </Link>
@@ -65,43 +65,43 @@ export function CardReleased({ releases }: { releases: Release[] }) {
 
                 {release.prerelease && (
                   <Badge
-                    className='text-yellow-600 border-yellow-600'
-                    variant='outline'
+                    className="text-yellow-600 border-yellow-600"
+                    variant="outline"
                   >
                     Pre-release
                   </Badge>
                 )}
               </section>
 
-              <section className='flex items-center pl-6 pb-6'>
+              <section className="flex items-center pl-6 pb-6">
                 <Image
                   src={release.author.avatar_url}
                   alt={release.author.avatar_url}
                   width={25}
                   height={25}
-                  className='flex scale-90 rounded-full border border-gray-800 mr-2'
+                  className="flex scale-90 rounded-full border border-gray-800 mr-2"
                 />
 
-                <span className='flex text-gray-400 text-sm font-bold mb-0.5 mr-2'>
+                <span className="flex text-gray-400 text-sm font-bold mb-0.5 mr-2">
                   <Link
                     href={release.author.html_url}
-                    className='hover:underline'
-                    rel='noopener noreferrer'
-                    target='_blank'
+                    className="hover:underline"
+                    rel="noopener noreferrer"
+                    target="_blank"
                   >
                     {release.author.login}
                   </Link>
                 </span>
 
                 <Badge
-                  className='flex text-gray-100 bg-gray-800 mr-6'
-                  variant='secondary'
+                  className="flex text-gray-100 bg-gray-800 mr-6"
+                  variant="secondary"
                 >
                   {timeSinceFormat(new Date(release.published_at))}
                 </Badge>
               </section>
 
-              <div className='border-b border-gray-600 py-1 mb-6 mx-6' />
+              <div className="border-b border-gray-600 py-1 mb-6 mx-6" />
 
               <CardContent>
                 <div
